d1f474e74d39e0d910535b306a989e2eee335e17
Sourav Anand <sauravanand324@gmail.com>
2024-04-18 20:50:26 +0530

    implemented teamController

---
+++ src/teams/team.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Team } from './team.entity';
+import { TeamService } from './team.service';
+
+@Controller('teams')
+export class TeamsController {
+    constructor(private teamService : TeamService) {}
+
+    @Post()
+    create(@Body() team : Team): Promise<Team> {
+        return this.teamService.create(team);
+    }
+
+    @Get()
+    findAll(): Promise<Team[]> {
+        return this.teamService.findAll();
+    }
+
+    @Get(':id')
+    findById(@Param('id') id:string): Promise<Team> {
+        return this.teamService.findById(id);
+    }
+}
