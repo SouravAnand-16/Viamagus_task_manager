29612580e57839e09bcede1a2ab4919e6cfccb6c
Sourav Anand <sauravanand324@gmail.com>
2024-04-18 19:47:14 +0530

    implemented taskservice

--- src/tasks/tasks.modules.ts
+++ src/tasks/tasks.modules.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common' ;
 import { TypeOrmModule } from '@nestjs/typeorm' ;
 import { Task } from './task.entity';
+import { TasksService } from './tasks.services';
 
 @Module({
-    imports : [TypeOrmModule.forFeature([Task])]
+    imports : [TypeOrmModule.forFeature([Task])],
+    providers: [TasksService],
 })
 export class TasksModule {}
--- src/tasks/tasks.services.ts
+++ src/tasks/tasks.services.ts
@@ -2,3 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Task } from './task.entity';
+
+@Injectable()
+export class TasksService {
+    constructor(
+      @InjectRepository(Task)
+      private taskRepository : Repository<Task>,
+    ){}
+
+    async findAll(): Promise<Task[]> {
+        return this.taskRepository.find();
+    }
+
+    async create(task: Task): Promise<Task> {
+        return this.taskRepository.save(task);
+      }
+    
+      async update(id: string, taskData: Partial<Task>): Promise<Task> {
+        await this.taskRepository.update(id, taskData as any);
+        return this.taskRepository.findOne(id) ;
+      }
+}
