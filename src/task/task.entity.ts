69c5927f3032be1ac9a038507d34c7fe03883b84
Sourav Anand <sauravanand324@gmail.com>
2024-04-18 19:10:51 +0530

    implemented task entity

---
+++ src/task/task.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, ObjectIdColumn, ObjectId, Column } from 'typeorm';
+
+@Entity()
+export class Task {
+    @ObjectIdColumn()
+    id: ObjectId;
+
+    @Column()
+    description: string;
+
+    @Column()
+    dueDate: Date;
+
+    @Column()
+    assignee: string;
+
+    @Column()
+    status: string;
+}
